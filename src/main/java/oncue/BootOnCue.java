08773c39f5b5cf827964d53b0ff429f844c98aab
Barry Kelly <barry@modeltwozero.com>
2013-03-15 15:19:48 +0000

    Add bootable class, configuration needed before actual boot up is possible

---
+++ src/main/java/oncue/BootOnCue.java
@@ -0,0 +1,13 @@
+package oncue;
+
+import akka.kernel.Bootable;
+
+public class BootOnCue implements Bootable {
+	@Override
+	public void startup() {
+	}
+
+	@Override
+	public void shutdown() {
+	}
+}
