2862cc71d4795fea0ce693d7eca37dede82edf73
Michael Marconi <michael@modeltwozero.com>
2013-06-18 14:26:21 +0100

    Bumping version to 0.9.1-SNAPSHOT
    Neglected to bump the Scala build version

--- oncue-service/project/Build.scala
+++ oncue-service/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "oncue-service"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "0.9.1-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
