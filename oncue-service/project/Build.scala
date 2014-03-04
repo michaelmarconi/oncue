f279be520c67229f1a91b69cd69c6ba58712a213
Michael Marconi <michael@modeltwozero.com>
2014-03-04 15:14:45 +0000

    Bump to 1.0.0-SNAPSHOT

--- oncue-service/project/Build.scala
+++ oncue-service/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "oncue-service"
-  val appVersion      = "0.9.6-SNAPSHOT"
+  val appVersion      = "1.0.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
