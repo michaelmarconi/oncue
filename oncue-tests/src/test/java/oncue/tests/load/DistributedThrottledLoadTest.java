802d71db843475836e980930070a17a176ff4d7a
Martin Foot <martin@modeltwozero.com>
2015-01-22 10:29:50 +0000

    Remove unused imports in DistributedThrottledLoadTest

--- oncue-tests/src/test/java/oncue/tests/load/DistributedThrottledLoadTest.java
+++ oncue-tests/src/test/java/oncue/tests/load/DistributedThrottledLoadTest.java
@@ -21,9 +21,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import oncue.agent.ThrottledAgent;
-import junit.framework.Assert;
-import oncue.agent.ThrottledAgent;
-import oncue.backingstore.RedisBackingStore;
 import oncue.common.messages.EnqueueJob;
 import oncue.common.messages.Job;
 import oncue.common.messages.Job.State;
